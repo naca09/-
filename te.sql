316095d28d51e2372522f885b05324694d9ac330
naca09 <86470016+naca09@users.noreply.github.com>
2023-12-05 20:18:33 +0700

    Update te.sql

--- te.sql
+++ te.sql
@@ -1,24 +1,6 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: 123.206.49.150    Database: cakeshop
--- ------------------------------------------------------
--- Server version	5.7.24-log
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `goods`
---
-
+CREATE database cakeshop;
+use cakeshop;
 DROP TABLE IF EXISTS `goods`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -37,16 +19,8 @@ CREATE TABLE `goods` (
   CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*Khóa ngoại này tham chiếu đến khóa chính của bảngtypetrên cộtid`.*/
 
---
--- Dumping data for table `goods`
---
-
-
-use sweetcake;
---
--- Table structure for table `order`
---
 
 DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -67,15 +41,7 @@ CREATE TABLE `order` (
 ) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `order`
---
 
-
-
---
--- Table structure for table `orderitem`
---
 
 DROP TABLE IF EXISTS `orderitem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -94,9 +60,7 @@ CREATE TABLE `orderitem` (
 ) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `orderitem`
---
+
 
 LOCK TABLES `orderitem` WRITE;
 /*!40000 ALTER TABLE `orderitem` DISABLE KEYS */;
@@ -104,9 +68,6 @@ INSERT INTO `orderitem` VALUES (43,50.2,1,1,47);
 /*!40000 ALTER TABLE `orderitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `recommend`
---
 
 DROP TABLE IF EXISTS `recommend`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -121,15 +82,7 @@ CREATE TABLE `recommend` (
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `recommend`
---
 
-
-
---
--- Table structure for table `type`
---
 
 DROP TABLE IF EXISTS `type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -141,14 +94,7 @@ CREATE TABLE `type` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `type`
---
 
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -169,24 +115,8 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
 
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-12-03 12:14:30
-INSERT INTO `user` (username, email, password, name, phone, address, isadmin, isvalidate)
+INSERT INTO `user` (`username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`)
 VALUES 
-('user1', 'user@gmail.com', 'password1', 'User One', '1234567890', 'Address 1', b'0', b'1'),
-('user2', 'admin@gmail.com', 'adminpassword', 'User Two', '0987654321', 'Address 2', b'1', b'1'),
-('user3', 'user3@example.com', 'password3', 'User Three', '1122334455', 'Address 3', b'0', b'0');
+('user1', 'user1@example.com', 'password1', 'User One', '1234567890', 'Address 1', b'0', b'1'),
+('user2', 'user2@example.com', 'password2', 'User Two', '0987654321', 'Address 2', b'1', b'0');
