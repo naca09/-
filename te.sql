63b6f574da97d2b858a6cb7324d0fce72d779764
naca09 <86470016+naca09@users.noreply.github.com>
2023-12-09 00:03:24 +0700

    Update te.sql

--- te.sql
+++ te.sql
@@ -1,4 +1,3 @@
-
 CREATE database cakeshop;
 use cakeshop;
 DROP TABLE IF EXISTS `goods`;
@@ -19,7 +18,7 @@ CREATE TABLE `goods` (
   CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=181 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*Khóa ngoại này tham chiếu đến khóa chính của bảngtypetrên cộtid`.*/
+/*Khóa ngoại này tham chiếu đến khóa chính của bảng type trên cột id`.*/
 
 
 DROP TABLE IF EXISTS `order`;
@@ -40,6 +39,10 @@ CREATE TABLE `order` (
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/***3. ON DELETE NO ACTION :**
+
+* Mệnh đề này trong các ràng buộc khóa ngoại chỉ định rằng việc xóa một hàng khỏi bảng được tham chiếu (ví dụ: `Order`) sẽ không tự động xóa bất kỳ order nào trong các bảng tham chiếu (ví dụ: `orderitem`) tham chiếu đến hàng đó.
+ Điều này đảm bảo tính toàn vẹn dữ liệu và ngăn chặn việc xóa tầng không mong muốn.*/
 
 
 
@@ -59,7 +62,10 @@ CREATE TABLE `orderitem` (
   CONSTRAINT `fk_orderitem_goods_id` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+/*Cụ thể, hai ràng buộc khóa ngoại fk_order_id và fk_orderitem_goods_id đều có mệnh đề ON DELETE NO ACTION.
+ Mệnh đề này quy định rằng việc xóa một hàng từ các bảng được tham chiếu (order và goods) sẽ không tự động xóa bất kỳ hàng nào trong bảng orderitem 
+ tham chiếu đến các hàng đó. Nói cách khác, ngay cả khi bạn cố gắng xóa một hàng hóa, bất kỳ đơn hàng nào tham chiếu đến hàng hóa đó sẽ vẫn còn trong cơ sở dữ liệu.
+*/
 
 
 LOCK TABLES `orderitem` WRITE;
@@ -119,4 +125,23 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`username`, `email`, `password`, `name`, `phone`, `address`, `isadmin`, `isvalidate`)
 VALUES 
 ('user1', 'user1@example.com', 'password1', 'User One', '1234567890', 'Address 1', b'0', b'1'),
-('user2', 'user2@example.com', 'password2', 'User Two', '0987654321', 'Address 2', b'1', b'0');
+('user2', 'admin@gmail.com', 'admin', 'User Two', '0987654321', 'Address 2', b'1', b'0');
+/*## Giải thích câu lệnh SQL:
+
+**2. Ràng buộc khóa ngoại:**
+
+* **`goods`.`type_id` tham chiếu `type`.`id`:** Ràng buộc này đảm bảo rằng `type_id` trong bảng `goods` trỏ đến ID hợp lệ trong bảng `type`.
+* **`order`.`user_id` tham chiếu `user`.`id`:** Ràng buộc này đảm bảo rằng `user_id` trong bảng `order` trỏ đến ID hợp lệ trong bảng `user`.
+* **`orderitem`.`goods_id` tham chiếu `goods`.`id`:** Ràng buộc này đảm bảo rằng `goods_id` trong bảng `orderitem` trỏ đến một ID hợp lệ trong bảng `goods`.
+* **`orderitem`.`order_id` tham chiếu `order`.`id`:** Ràng buộc này đảm bảo rằng `order_id` trong bảng `orderitem` trỏ đến ID hợp lệ trong bảng `order`.
+* **`recommend`.`goods_id` tham chiếu `goods`.`id`:** Ràng buộc này đảm bảo rằng `goods_id` trong bảng `recommend` trỏ đến ID hợp lệ trong bảng `goods`.
+
+**3. ON DELETE NO ACTION :**
+
+* Mệnh đề này trong các ràng buộc khóa ngoại chỉ định rằng việc xóa một hàng khỏi bảng được tham chiếu (ví dụ: `hàng hóa`) sẽ không tự động xóa bất kỳ hàng nào trong các bảng tham chiếu (ví dụ: `orderitem`) tham chiếu đến hàng đó. Điều này đảm bảo tính toàn vẹn dữ liệu và ngăn chặn việc xóa tầng không mong muốn.
+
+**4. Dữ liệu mẫu:**
+
+* Một mục nhập dữ liệu mẫu được chèn vào bảng `orderitem`, liên kết một mặt hàng cụ thể với một đơn đặt hàng.
+
+**/
